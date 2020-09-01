7419e6c9b4e6625510072287eec9ae29713c0ab1
Marisa You <marisa.you@gmail.com>
2020-09-01 20:09:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
     if smallest == nil
       smallest = value
     elsif value < smallest
-      smallest = value
+      smallest = key
     end
   end
   return smallest
